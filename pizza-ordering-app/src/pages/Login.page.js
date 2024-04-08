f8e288b2b56a5be26630d773e9ef023868128c11
AdonisAlgos <adonisgeorgiou@hotmail.co.uk>
2024-04-08 19:05:47 +0100

    finalised initial login styles

--- pizza-ordering-app/src/pages/Login.page.js
+++ pizza-ordering-app/src/pages/Login.page.js
@@ -3,18 +3,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const LoginPage = () => {
   return (
-    <form>
-      <div class="form-group">
-        <input type="text" class="form-control" placeholder="Username" />
+    <div class="container vh-100 d-flex align-items-center justify-content-center">
+      <div class="row w-100">
+        <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4 mx-auto">
+          <h1 class="text-center mb-4">Welcome</h1>
+          <form>
+            <div class="form-group mb-3">
+              <input type="text" class="form-control" placeholder="Username" />
+            </div>
+            <div class="form-group mb-3">
+              <input
+                type="password"
+                class="form-control"
+                placeholder="Password"
+              />
+            </div>
+            <div class="d-flex justify-content-center">
+              <button class="btn btn-primary me-2">Login</button>
+              <button class="btn btn-primary">Register</button>
+            </div>
+          </form>
+        </div>
       </div>
-      <div class="form-group">
-        <input type="password" class="form-control" placeholder="Password" />
-      </div>
-      <div class="d-flex justify-content-center">
-        <button class="btn btn-primary me-2">Login</button>
-        <button class="btn btn-primary">Register</button>
-      </div>
-    </form>
+    </div>
   );
 };
 
