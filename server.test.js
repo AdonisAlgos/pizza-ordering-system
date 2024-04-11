b70971d46974ca485538a3bbda008d7328106312
AdonisAlgos <adonisgeorgiou@hotmail.co.uk>
2024-04-11 16:20:30 +0100

    updated server-side tests

--- server.test.js
+++ server.test.js
@@ -1,70 +1,3 @@
-// const request = require("supertest");
-// const { app } = require("./server");
-// const UserModel = require("./models/User.model.js");
-
-// describe("POST /register", () => {
-//   afterEach(async () => {
-//     // Delete only the test user to avoid affecting other data
-//     await UserModel.deleteMany({ email: /.*test@example\.com$/ });
-//   }, 20000);
-
-//   it("should return 400 for missing fields", async () => {
-//     const response = await request(app)
-//       .post("/register")
-//       .send({ name: "Test" }); // Incomplete data for testing
-
-//     expect(response.statusCode).toBe(400);
-//     expect(response.body).toHaveProperty("message", "All fields are required");
-//   });
-
-//   it("should return 201 for successful registration", async () => {
-//     const testEmail = "jane.doe.test@example.com";
-//     const response = await request(app).post("/register").send({
-//       name: "Jane",
-//       lastName: "Doe",
-//       email: testEmail,
-//       password: "123456",
-//       streetAddress: "123 Main St",
-//       city: "Anytown",
-//       postcode: "12345",
-//     });
-
-//     expect(response.statusCode).toBe(201);
-//     expect(response.body).toHaveProperty(
-//       "message",
-//       "User registered successfully"
-//     );
-//   });
-
-//   it("should return 409 for duplicate email", async () => {
-//     const testEmail = "john.doe.test@example.com";
-//     // Create a user for the duplicate check
-//     await request(app).post("/register").send({
-//       name: "John",
-//       lastName: "Doe",
-//       email: testEmail,
-//       password: "password123",
-//       streetAddress: "456 Elm St",
-//       city: "Townsville",
-//       postcode: "54321",
-//     });
-
-//     // Attempt to create the same user again
-//     const response = await request(app).post("/register").send({
-//       name: "John",
-//       lastName: "Doe",
-//       email: testEmail, // Same email to trigger the duplicate error
-//       password: "password123",
-//       streetAddress: "456 Elm St",
-//       city: "Townsville",
-//       postcode: "54321",
-//     });
-
-//     expect(response.statusCode).toBe(409);
-//     expect(response.body).toHaveProperty("message", "User already exists");
-//   });
-// });
-
 const request = require("supertest");
 const { app } = require("./server");
 const mongoose = require("mongoose");
@@ -75,7 +8,6 @@ dotenv.config();
 
 // Setup a test database connection
 beforeAll(async () => {
-
   await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -93,6 +25,15 @@ afterAll(async () => {
 });
 
 describe("User Registration", () => {
+  it("should return 400 for missing fields", async () => {
+    const response = await request(app)
+      .post("/register")
+      .send({ name: "Test" }); // Incomplete data for testing
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("message", "All fields are required");
+  });
+
   test("It should register a new user", async () => {
     const newUser = {
       name: "John",
