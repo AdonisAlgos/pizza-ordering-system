28b0e7d6214babe32d34a98b670bbdbc49dd45dd
AdonisAlgos <adonisgeorgiou@hotmail.co.uk>
2024-04-12 12:04:14 +0100

    added pizza grid

--- pizza-ordering-app/src/pages/Home.page.js
+++ pizza-ordering-app/src/pages/Home.page.js
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <div>
       <h1
-        className="d-flex align-items-center justify-content-center"
+        className="d-flex align-items-center justify-content-center text-center"
         style={{
           fontFamily: "'Pacifico', cursive",
           height: "150px",
@@ -12,8 +12,19 @@ const HomePage = () => {
       >
         Best Stone Oven Pizza in London
       </h1>
+      {/* Grid starts here */}
+      <div className="container mt-4">
+        <div className="row">
+          {[...Array(4)].map((_, index) => (
+            <div key={index} className="col-lg-3 col-md-4 col-sm-6 col-12 mb-3">
+              <div className="bg-danger p-3">Item {index + 1}</div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
-}
+};
+
 
 export default HomePage
