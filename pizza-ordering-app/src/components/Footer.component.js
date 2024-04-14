2f2d2a812bc45e7ea799cefcf37ea76c050c21b5
AdonisAlgos <adonisgeorgiou@hotmail.co.uk>
2024-04-15 00:48:40 +0100

    addressed build issues

--- pizza-ordering-app/src/components/Footer.component.js
+++ pizza-ordering-app/src/components/Footer.component.js
@@ -11,14 +11,6 @@ const FooterComponent = () => {
     <div className="container">
       <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <div className="col-md-4 d-flex align-items-center">
-          <a
-            className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
-            href="/" // Assuming you might want a link here to the home page or elsewhere
-          >
-            {/* Placeholder if you want to include an icon or logo */}
-            <FontAwesomeIcon icon={faTwitter} size="lg" />{" "}
-            {/* Example: Twitter icon used as logo */}
-          </a>
           <span className="mb-3 mb-md-0 text-muted">
             © 2024 Pizza Company, Inc
           </span>
@@ -26,17 +18,17 @@ const FooterComponent = () => {
 
         <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
           <li className="ms-3">
-            <a className="text-muted" href="#">
+            <a className="text-muted">
               <FontAwesomeIcon icon={faTwitter} size="lg" />
             </a>
           </li>
           <li className="ms-3">
-            <a className="text-muted" href="#">
+            <a className="text-muted">
               <FontAwesomeIcon icon={faInstagram} size="lg" />
             </a>
           </li>
           <li className="ms-3">
-            <a className="text-muted" href="#">
+            <a className="text-muted">
               <FontAwesomeIcon icon={faFacebook} size="lg" />
             </a>
           </li>
